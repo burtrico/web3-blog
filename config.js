94bd4b9555fcc628ef19d8f98e1a5cf2847c23e6
burtrico <burtrico@gmail.com>
2022-10-20 23:09:23 -0500

    deployed contract to mumbai

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  export const contractAddress = "0x8689f922178fb60af319780180e3F62d2003AF6f"
+  export const ownerAddress = "0x8759B07C01Cc9808Ecedf7c0A5ED68f176B9c012"
   
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,12 @@ const hre = require("hardhat");
 const fs = require('fs');
 
 async function main() {
+  // let hardCodedUser = '0x8759B07C01Cc9808Ecedf7c0A5ED68f176B9c012';
+
+  // [hardCodedUser] = ethers.getSigners();
+  // hardCodedUser.connect(ethers.provider)
+  // const signer = new ethers.Wallet(your_private_key_string);
+
   /* these two lines deploy the contract to the network */
   const Blog = await hre.ethers.getContractFactory("Blog");
   const blog = await Blog.deploy("My blog");
